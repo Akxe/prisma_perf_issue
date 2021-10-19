314fe6bf823f7786640bf7dc57b33a3e07fec542
Adam Eisenreich <akxe@seznam.cz>
2021-10-19 20:05:22 +0200

    initial

---
+++ prisma/seed.ts
@@ -0,0 +1,131 @@
+import { PrismaClient } from '@prisma/client';
+import { address, datatype } from 'faker';
+
+const prisma = new PrismaClient();
+const carrierCountInOffer = [250, 150, 100];
+const rowCounts: Record<Exclude<keyof typeof prisma, `$${string}`>, number> = {
+	carrier: 92_500,
+	period: 120,
+	offer: 3_500,
+
+	//	Approximate row count in our real DB
+	periodsOfOffer: 31_500,
+	carrierOfOffer: 220_000,
+	carrierInPeriod: 1_110_000,
+};
+
+const bulkSize = 1000;
+async function asyncForEach<T>(arr: T[], callback: (item: T[], index: number) => Promise<any>) {
+	const bulks: T[][] = [];
+
+	for (let i = 0; i < Math.ceil(arr.length / bulkSize); i++) {
+		bulks.push(arr.slice(i * bulkSize, (i + 1) * bulkSize));
+	}
+
+	for (let index = 0; index < bulks.length; index++) {
+		await callback(bulks[index], index);
+	}
+}
+
+// A `main` function so that you can use async/await
+async function main() {
+	await Promise.all([
+		prisma.carrier.deleteMany(),
+		prisma.periodsOfOffer.deleteMany(),
+		prisma.carrierOfOffer.deleteMany(),
+		prisma.carrierInPeriod.deleteMany(),
+	]);
+
+	await asyncForEach(
+		Array.from({ length: rowCounts.carrier }, (_, index) => {
+			return {
+				carrierID: index + 1,
+				city: address.cityName(),
+				street: address.streetAddress(),
+			};
+		}),
+		data => prisma.carrier.createMany({ data }),
+	);
+
+	await prisma.period.createMany({
+		data: Array.from({ length: rowCounts.period }, (_, index) => {
+			return {
+				periodID: index + 1,
+				text: `${index % 12 + 1}/${Math.floor(index / 12) + 2013}`,
+			};
+		}),
+		skipDuplicates: true,
+	});
+
+	await prisma.offer.createMany({
+		data: Array.from({ length: rowCounts.offer }, (_, index) => {
+			return { offerID: index + 1 };
+		}),
+		skipDuplicates: true,
+	});
+
+	const data = Array.from({ length: rowCounts.offer }, (_, index) => {
+		const offerID = index + 1;
+		const startPeriodID = datatype.number({
+			min: 1,
+			max: rowCounts.period - 12,
+		});
+
+		//	Needed average of 35
+		const numberOfCarrierInOffer = carrierCountInOffer[offerID - 1] ?? datatype.number({ min: 10, max: 60 });
+
+		const periodsOfOffer = Array.from({ length: datatype.number({ min: 6, max: 12 }), }, (_, index) => {
+			const periodID = startPeriodID + index;
+
+			return {
+				periodsOfOffer: { offerID, periodID },
+				carrierInPeriod: Array.from({ length: numberOfCarrierInOffer }, (_, index) => {
+					return {
+						offerID,
+						periodID,
+						carrierID: startPeriodID + index,
+
+						selected: datatype.boolean(),
+						wanted: datatype.boolean(),
+					};
+				}),
+			};
+		});
+
+		return {
+			//	Needed average of 9
+			periodsOfOffer: periodsOfOffer.flatMap(res => res.periodsOfOffer),
+			carrierOfOffer: Array.from({ length: numberOfCarrierInOffer }, (_, index) => {
+				return {
+					offerID,
+					carrierID: startPeriodID + index,
+					shortID: index + 1,
+				};
+			}),
+			carrierInPeriod: periodsOfOffer.flatMap(res => res.carrierInPeriod),
+		};
+	});
+
+	await asyncForEach(
+		data.flatMap(row => row.periodsOfOffer),
+		data => prisma.periodsOfOffer.createMany({ data }),
+	);
+
+	await asyncForEach(
+		data.flatMap(row => row.carrierOfOffer),
+		data => prisma.carrierOfOffer.createMany({ data }),
+	);
+
+	await asyncForEach(
+		data.flatMap(row => row.carrierInPeriod),
+		data => prisma.carrierInPeriod.createMany({ data }),
+	);
+}
+
+main()
+	.catch(e => {
+		throw e
+	})
+	.finally(async () => {
+		await prisma.$disconnect()
+	})
---
+++ script.ts
@@ -0,0 +1,87 @@
+import { PrismaClient } from '@prisma/client'
+import { inspect } from 'util';
+
+const prisma = new PrismaClient();
+
+function timePromise<T>(description: string, promise: T | Promise<T>): Promise<T> {
+	if (!(promise instanceof Promise)) {
+		promise = Promise.resolve(promise);
+	}
+
+	console.time(description);
+	promise.finally(() => console.timeEnd(description));
+
+	return promise;
+}
+
+// A `main` function so that you can use async/await
+async function main() {
+	const offerIDs = [1];
+	const [offer, period, carrierOfOffer] = await Promise.all([
+		timePromise('offer', prisma.offer.findMany({
+			select: {
+				offerID: true,
+			},
+			where: {
+				offerID: {
+					in: offerIDs,
+				},
+			},
+		})),
+		timePromise('period', prisma.period.findMany({
+			select: {
+				periodID: true,
+				text: true,
+			},
+			where: {
+				periodsOfOffer: {
+					some: {
+						offerID: {
+							in: offerIDs,
+						},
+					},
+				},
+			},
+		})),
+		timePromise('carrierOfOffer', prisma.carrierOfOffer.findMany({
+			select: {
+				offerID: true,
+				carrierID: true,
+				shortID: true,
+				carrier: {
+					select: {
+						city: true,
+						street: true,
+					},
+				},
+				periods: {
+					select: {
+						periodID: true,
+						selected: true,
+						wanted: true,
+					},
+				},
+			},
+			where: {
+				offerID: {
+					in: offerIDs,
+				},
+			},
+		})),
+	]);
+
+	console.log({
+		offer,
+		period,
+		//	Showing first 10
+		carrierOfOffer: inspect(carrierOfOffer.slice(0, 10), { showHidden: false, depth: null, colors: true }),
+	});
+}
+
+main()
+	.catch(e => {
+		throw e
+	})
+	.finally(async () => {
+		await prisma.$disconnect()
+	})
